b4bfcf192482f82ca84172fb884e62dac3c1ab4a
Akshay095 <56989557+Akshay095@users.noreply.github.com>
2019-10-25 09:38:12 +0530

    Add files via upload

---
+++ cont.py
@@ -0,0 +1,28 @@
+
+class Person:
+    key=123
+    
+
+    def __init__(self,k):
+        
+          
+       if Person.key==k:
+              print("constructor accessed")
+              self.name="Akshay"
+       else:
+           print("can't access the constructor")
+                
+
+    def printH(self):        
+        print("Welcome")
+        print(self.name)
+        
+
+abc=Person(123)
+try:
+    abc.printH()
+except Exception as s:
+    print(s)
+    
+
+
